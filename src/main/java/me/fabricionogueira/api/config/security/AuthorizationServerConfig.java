70f84f19d87c7f702eee75b0c2b859a6eb49f46c
Fabricio Nogueira <fabricio.santos@treasy.com.br>
2018-12-27 14:25:02 -0200

    feat(JWT): Improve jwt oauth with spring authentication

--- src/main/java/me/fabricionogueira/api/config/security/AuthorizationServerConfig.java
+++ src/main/java/me/fabricionogueira/api/config/security/AuthorizationServerConfig.java
@@ -3,7 +3,6 @@ package me.fabricionogueira.api.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -11,6 +10,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 @Configuration
 @EnableAuthorizationServer
@@ -26,10 +27,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	private String secret;
 
 	private AuthenticationManager authenticationManager;
+	private DefaultTokenServices defaultTokenServices;
+	private AccessTokenConverter accessTokenConverter;
 
 	@Autowired
-	public AuthorizationServerConfig(AuthenticationManager authenticationManager) {
+	public AuthorizationServerConfig(
+		AuthenticationManager authenticationManager,
+		DefaultTokenServices defaultTokenServices,
+		AccessTokenConverter accessTokenConverter) {
+
 		this.authenticationManager = authenticationManager;
+		this.defaultTokenServices = defaultTokenServices;
+		this.accessTokenConverter = accessTokenConverter;
 	}
 
 	@Override
@@ -54,7 +63,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager)
-			.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+		endpoints
+			.tokenServices(defaultTokenServices)
+			.authenticationManager(authenticationManager)
+			.accessTokenConverter(accessTokenConverter);
 	}
 }
