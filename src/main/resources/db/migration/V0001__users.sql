97541bc89da2382921f19be0d3479ba402dc793c
Fabricio Nogueira <fabricio.santos@treasy.com.br>
2018-12-27 14:29:53 -0200

    refactor(Files): Remove unused files

--- src/main/resources/db/migration/V0001__users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id      bigserial NOT NULL,
-  name    varchar(99) NOT NULL,
-  email   varchar(99) NOT NULL,
-  password   varchar(99) NOT NULL,
-  active   BOOLEAN DEFAULT TRUE,
-  CONSTRAINT user_pk PRIMARY KEY (id)
-);
